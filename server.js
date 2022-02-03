f3d692a7020879f9282f4c800d57e44f06a5c022
Julie Carra <julie.carra@live.fr>
2022-02-03 19:32:59 +0100

    makdeploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const passport = require("passport");
+const path = require("path");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 
@@ -43,6 +44,13 @@ app.use("/api/bookings", require("./routes/bookings"));
 app.use("/api/payments", require("./routes/payments"));
 
 require("./middlewares/passport");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(process.env.PORT, () => {
   console.log(`App listening on: http://localhost:${process.env.PORT}/ !`);
